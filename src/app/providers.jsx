64b60f5f754c87a9e689a3105a7ad270cae888ac
haseebcodejourney <haseebcodejourney@gmail.com>
2026-05-08 17:28:53 +0300

    update

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import store from '../store';
+import store from '../Store';
 import { router } from './router';
 
 export default function Providers() {
